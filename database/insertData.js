6ac34344ad0bf7e03309d6ae2a8804a7640f0e0b
Thomas Brock <thomasbrock@sfs-wifi-dhcp-10-143-71-16.wifi.sfsu.edu>
2024-05-02 10:54:43 -0700

    Made insertData more secure.

--- database/insertData.js
+++ database/insertData.js
@@ -2,10 +2,12 @@ const db = require("./db.js");
 
 function insertRow(username, password) {
   db.run(
-    `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`,
+    `INSERT INTO users (username, password) VALUES (?, ?)`,
+    [username, password],
     function (error) {
       if (error) {
         console.error(error.message);
+        throw new Error(error.message);
       }
       console.log(`Inserted a row with the ID: ${this.lastID}`);
     }
