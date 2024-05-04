fce3a2ec35f10ec6147d539f0a655b1583ca0ffd
ThomasSFSU <tmolnarbrock@sfsu.edu>
2024-05-04 11:28:48 -0700

    Required authentication for admin route. NOTE: more security needed in production.

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -2,16 +2,24 @@ const database = require('../database/Database');
 
 
 const addProduct = async (req, res) => {
-    const name = req.body.product_name;
-    const description = req.body.product_description;
-    const price = req.body.product_price;
-    const url = req.body.product_img;
-    console.log("body: ", req.body);
-    console.log("name: ", name);
-    console.log("description: ", description);
-    console.log("price: ", price);
-    console.log("img url: ", url);
-    database.insertProduct(name, description, price, url);
+    // Check login status
+    const isLoggedIn = req.session.isLoggedIn;
+    const username = req.session.username;
+    if (isLoggedIn && username === "ADMIN") {
+        const name = req.body.product_name;
+        const description = req.body.product_description;
+        const price = req.body.product_price;
+        const url = req.body.product_img;
+        console.log("body: ", req.body);
+        console.log("name: ", name);
+        console.log("description: ", description);
+        console.log("price: ", price);
+        console.log("img url: ", url);
+        database.insertProduct(name, description, price, url);
+    } else {
+        console.log("Must be admin to add a product.");
+        res.redirect('/login');
+    }
 }
 
 module.exports = {addProduct};
