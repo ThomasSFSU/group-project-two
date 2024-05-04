d8694e3c7a1ccc95aff2114e9dadf26b31404f91
ThomasSFSU <tmolnarbrock@sfsu.edu>
2024-05-03 22:02:31 -0700

    Changed layout and directory structure.

--- routes/checkout.js
+++ routes/checkout.js
@@ -5,7 +5,6 @@ const path = require('path');
 router.get('/', (req, res) => {
     // Check login status
     const isLoggedIn = req.session.isLoggedIn;
-    //const username = req.session.username;
     if (isLoggedIn) {
         const data = {username: req.session.username}
         console.log("Logged in Username in checkout.js: ", data);
