725bd0c0a80839be79cf7e197c476b9c5a64c54a
ThomasSFSU <tmolnarbrock@sfsu.edu>
2024-05-19 21:05:10 -0700

    Removed test code.

--- server.js
+++ server.js
@@ -55,27 +55,4 @@ app.get('/*', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
-})
-
-/// REMOVE
-var http = require("http");
-
-http.createServer(function(req, res) {
-  // Homepage
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Welcome to the homepage!");
-  }
-
-  // About page
-  else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Welcome to the about page!");
-  }
-
-  // 404'd!
-  else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 error! File not found.");
-  }
-}).listen(1337, "localhost");
+});
